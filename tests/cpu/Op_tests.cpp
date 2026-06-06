ba06cfb5933f8beb5838002be823352cbfc49d28
Mei Chu <meimchu@gmail.com>
2026-06-05 23:21:01 -0700

    Fix test
    Signed-off-by: Mei Chu <meimchu@gmail.com>

--- tests/cpu/Op_tests.cpp
+++ tests/cpu/Op_tests.cpp
@@ -516,6 +516,6 @@ OCIO_ADD_TEST(OpRcPtrVec, serialize)
     OCIO_CHECK_NO_THROW(OCIO::SerializeOpVec(ops));
 
     // Check Serialize output's prefix for indentation.
-    std::string_view ops_serialized(SerializeOpVec(ops, 4), 7);
+    std::string_view ops_serialized(OCIO::SerializeOpVec(ops, 4), 7);
     OCIO_CHECK_EQUAL(ops_serialized, "    Op ");
 }
