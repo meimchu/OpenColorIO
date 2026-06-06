91c004401cbd9764bfe8278b9d2e36fbd7c95139
Mei Chu <meimchu@gmail.com>
2026-06-05 22:55:35 -0700

    Add SerializeOpVec output prefix check.
    Signed-off-by: Mei Chu <meimchu@gmail.com>

--- tests/cpu/Op_tests.cpp
+++ tests/cpu/Op_tests.cpp
@@ -514,4 +514,7 @@ OCIO_ADD_TEST(OpRcPtrVec, serialize)
 
     // Serialize not optimized OpVec i.e. contains some NoOps.
     OCIO_CHECK_NO_THROW(OCIO::SerializeOpVec(ops));
+
+    // Check Serialize output's prefix for indentation.
+    OCIO_CHECK_EQUAL(std::string_view(SerializeOpVec(ops, 4), 7), "    Op ");
 }
